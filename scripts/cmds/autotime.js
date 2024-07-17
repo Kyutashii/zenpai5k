2cb0fcd094d40cfecd19c08e081339a3a6b67281
Kyledev033 <kyledev40@gmail.com>
2024-07-17 13:23:51 +0800

    Update autotime.js

--- scripts/cmds/autotime.js
+++ scripts/cmds/autotime.js
@@ -1,7 +1,13 @@
 const moment = require('moment-timezone');
-
-let switchEnabled = false;
-
+module.exports.config = {
+  name: "autotime",
+  version: "2.0.0",
+  role: 0,
+  author: "kylepogi",//don't change the author kung ayaw mong ma pwetan!! 
+  description: "Automatically sends messages based on set times.",
+  category: "AutoTime",
+  countDown: 3
+      },
 const arrayData = {
   "12:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 12:00 𝐏𝐌\n\n📌 good afternoon everyone don't forget to eat y'all lunch break🍛"
@@ -99,68 +105,28 @@ const arrayData = {
       "00:05:00": {
       message: "𝚃𝚑𝚒𝚜 𝚒𝚜 𝙰𝚞𝚝𝚘 𝚜𝚎𝚗𝚍 𝚖𝚎𝚜𝚜𝚊𝚐𝚎 𝚎𝚟𝚎𝚛𝚢 𝟻𝚖𝚒𝚗𝚞𝚝𝚎𝚜:\n\n𝚃𝚑𝚒𝚜 𝚒𝚜 𝚖𝚢 𝚘𝚠𝚗𝚎𝚛/𝚊𝚍𝚖𝚒𝚗 𝚊𝚌𝚌𝚘𝚞𝚗𝚝: https://www.facebook.com/kyledev03"
       }
-  // Add more messages for other times as needed
-};
+    // Add more messages for other times as needed
+  };
 
-module.exports = {
-  config: {
-    name: "autotime",
-    version: "2.0.0",
-    role: 0,
-    author: "kylepogi",
-    description: "Automatically sends messages based on set times.",
-    category: "AutoTime",
-    countDown: 3,
-    commands: [
-      {
-        command: "switch",
-        description: {
-          en: "Turn on/off automatic Schedule time on threads"
-        },
-        syntax: {
-          vi: "switch [on|off]",
-          en: "switch [on|off]"
-        }
-      }
-    ]
-  },
+  const checkTimeAndSendMessage = () => {
+    const now = moment().tz('Asia/Manila');
+    const currentTime = now.format('hh:mm:ss A');
 
-  onLoad: ({ api }) => {
-    const checkTimeAndSendMessage = () => {
-      const now = moment().tz('Asia/Manila');
-      const currentTime = now.format('hh:mm:ss A');
+    const messageData = arrayData[currentTime];
 
-      const messageData = arrayData[currentTime];
-
-      if (messageData && switchEnabled) {
-        const tid = global.db.allThreadData.map(i => i.threadID);
-        tid.forEach(async (threadID, index) => {
-          api.sendMessage({ body: messageData.message }, threadID);
-        });
-      }
-
-      const nextMinute = moment().add(1, 'minute').startOf('minute');
-      const delay = nextMinute.diff(moment());
-      setTimeout(checkTimeAndSendMessage, delay);
-    };
-
-    checkTimeAndSendMessage();
-  },
-
-  onStart: () => {},
-
-  onMessage: ({ message, args }) => {
-    if (message.body.startsWith('switch')) {
-      const command = args[0].toLowerCase();
-      if (command === 'on') {
-        switchEnabled = true;
-        message.reply("🚨 Auto Time ON 🟢\n\n⚠ AutoTime has been enabled.");
-      } else if (command === 'off') {
-        switchEnabled = false;
-        message.reply("🚨 Auto Time OFF 🔴\n\n⚠ AutoTime has been disabled.");
-      } else {
-        message.reply("Usage: switch [on|off]");
-      }
+    if (messageData) {
+      const tid = global.db.allThreadData.map(i => i.threadID);
+      tid.forEach(async (threadID, index) => {
+        api.sendMessage({ body: messageData.message }, threadID);
+      });
     }
-  }
+
+    const nextMinute = moment().add(1, 'minute').startOf('minute');
+    const delay = nextMinute.diff(moment());
+    setTimeout(checkTimeAndSendMessage, delay);
+  };
+
+  checkTimeAndSendMessage();
 };
+
+module.exports.onStart = () => {};
