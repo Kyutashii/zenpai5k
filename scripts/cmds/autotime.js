345803651b60871d8b8b7eff084131341aa11969
Kyledev033 <kyledev40@gmail.com>
2024-07-18 20:42:04 +0800

    Update autotime.js

--- scripts/cmds/autotime.js
+++ scripts/cmds/autotime.js
@@ -1,10 +1,10 @@
-const moment = require('moment-timezone');
+const { MessageAttachment } = require('facebook-chat-api');
 
 module.exports.config = {
   name: "autotime",
   version: "2.0.0",
   role: 0,
-  author: "kylepogi",//don't change the author kung ayaw mong ma pwetan!! 
+  author: "kylepogi", //lol change author pa kinginamo
   description: "Automatically sends messages based on set times.",
   category: "AutoTime",
   countDown: 3
@@ -12,103 +12,119 @@ module.exports.config = {
 
 module.exports.onLoad = async ({ api }) => {
   const arrayData = {
-  "12:00:00 PM": {
+    "12:00:00 PM": {
+      message: "🔔 𝗔𝘂𝘁𝗼 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 12:00 𝐏𝐌\n\n📌 good afternoon everyone don't forget to eat y'all lunch break🍛"
+      attachment:  'https://i.imgur.com/hg9AuMr.gif'// Add attachments here if needed
+    },
+    "01:00:00 AM": {
+      message: "🔔 𝗔𝘂𝘁𝗼 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 01:00 𝐀𝐌\n\n📌 good morning everyone!!, have a nice morning🍞☕🌅"
+      attachment: 'https://i.imgur.com/kNIfj76.gif',
+    },
+         },
+      "02:00:00 AM": {
+        message: "🔔 𝗔𝘂𝘁𝗼 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 02:00 𝐀𝐌\n\n📌 don't forget to add/follow my owner☺.\n\n📩: https://www.facebook.com/kyledev03"
+attachment: ''
+      },
+      "03:00:00 AM": {
+        message: "🔔 𝗔𝘂𝘁𝗼 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 03:00 𝐀𝐌\n\n📌 aga nyo nagising ahh"
+        attachment: 'https://i.imgur.com/kNIfj76.gif'
+      },
+   "12:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 12:00 𝐏𝐌\n\n📌 good afternoon everyone don't forget to eat y'all lunch break🍛"
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "01:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 01:00 𝐀𝐌\n\n📌 good morning everyone!!, have a nice morning🍞☕🌅"
       },
       "02:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 02:00 𝐀𝐌\n\n📌 don't forget to add/follow my owner☺.\n\n📩: https://www.facebook.com/kyledev03"
-
+attachment:  ''
       },
       "03:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 03:00 𝐀𝐌\n\n📌 aga nyo nagising ahh"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "04:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 04:00 𝐀𝐌\n\n📌  eyyy🤙don't panic it's organic eyyyyy🤙"
-
+attachment:  ''
       },
       "05:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 05:00 𝐀𝐌\n\n📌 aga nyo nagising ahh sanaol strong💀🙏"
-        
+        attachment:  'https://i.imgur.com/HAxmlRq.gif'
       },
       "06:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 06:00 𝐀𝐌\n\n📌 kape muna kayo☕"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "07:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 07:00 𝐀𝐌\n\n📌 don't forget to eat y'all breakfast!! 🍞☕🍛"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "08:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 08:00 𝐀𝐌\n\n📌 life update: pogi parin owner ko"
-        
+        attachment:  'https://i.imgur.com/LBSQkVV.mp4'
       },
       "09:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 09:00 𝐀𝐌\n\n📌 baka hinde pa kayo kumain kain na kayo💀🙏"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "10:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 10:00 𝐀𝐌\n\n📌 wag mo kalimutan e chat owner ko💀🙏"
-        
+        attachment:  ''
       },
       "11:00:00 AM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 11:00 𝐀𝐌\n\n📌  hinde mababawasan kapogian ng owner ko, btw have a nice morning everyone!!"
-        
+        attachment:  'https://i.imgur.com/LBSQkVV.mp4'
       },
       "12:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 12:00 𝐏𝐌\n\n📌  kain na kayo mga lods💀"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "01:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 01:00 𝐏𝐌\n\n📌 dont forget to eat y'all launchbreak😸"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "02:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 02:00 𝐏𝐌\n\n📌 good afternoon!!,my owner is so handsome asf😎 "
-        
+        attachment:  'https://i.imgur.com/LBSQkVV.mp4'
       },
       "03:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 03:00 𝐏𝐌\n\n 📌 miss ko na sya:("
-        
+        attachment:  'https://i.imgur.com/bxaMPz8.jpeg'
       },
       "04:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 04:00 𝐏𝐌\n\n📌 magandang hapon mga lods😸"
-        
+        attachment:  'https://i.imgur.com/f3KxFnV.gif'
       },
       "05:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 05:00 𝐏𝐌\n\n📌 pogi ng owner ko na si Kyle 😎"
-        
+        attachment:  'https://i.imgur.com/LBSQkVV.mp4'
       },
       "06:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 06:00 𝐏𝐌\n\n📌 don't forget to eat y'all dinner💀🙏"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "07:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 07:00 𝐏𝐌\n\n📌 ano silbe ng pag online mo kung hinde mo din naman e chachat owner ko😎"
-        
+        attachment:  'https://i.imgur.com/LBSQkVV.mp4'
       },
       "08:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 08:00 𝐏𝐌\n\n📌 kumain naba kayo?"
-        
+        attachment:  'https://i.imgur.com/hg9AuMr.gif'
       },
       "09:00:00 PM": {
-        message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 09:00 𝐏𝐌\n\n📌 matulog na kayo mga hangal😸"
-        
+        message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 09:00 𝐏𝐌\n\n📌 it's time to sleep, good night."
+        attachment:  'https://i.imgur.com/QOmMPwC.gif'
       },
       "10:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 10:00 𝐏𝐌\n\n📌 gabi na nag puyat parin kayo💀🙏"
-        
+        attachment:  'https://i.imgur.com/HAxmlRq.gif'
       },
       "11:00:00 PM": {
         message: "🔔 𝗔𝘂𝘁𝗼 𝗧𝗶𝗺𝗲:\n▬▬▬▬▬▬▬▬▬▬▬▬\n⏰ time now - 11:00 𝐏𝐌\n\n📌 hinde mababawasan kapogian ng owner ko."
-      }, 
-      "00:05:00": {
-      message: "𝚃𝚑𝚒𝚜 𝚒𝚜 𝙰𝚞𝚝𝚘 𝚜𝚎𝚗𝚍 𝚖𝚎𝚜𝚜𝚊𝚐𝚎 𝚎𝚟𝚎𝚛𝚢 𝟻𝚖𝚒𝚗𝚞𝚝𝚎𝚜:\n\n𝚃𝚑𝚒𝚜 𝚒𝚜 𝚖𝚢 𝚘𝚠𝚗𝚎𝚛/𝚊𝚍𝚖𝚒𝚗 𝚊𝚌𝚌𝚘𝚞𝚗𝚝: https://www.facebook.com/kyledev03"
-      }
-    // Add more messages for other times as needed
+        attachment:  'https://i.imgur.com/LBSQkVV.mp4'
+     }
+    // Add attachments for other messages similarly
   };
 
   const checkTimeAndSendMessage = () => {
@@ -120,7 +136,12 @@ module.exports.onLoad = async ({ api }) => {
     if (messageData) {
       const tid = global.db.allThreadData.map(i => i.threadID);
       tid.forEach(async (threadID, index) => {
-        api.sendMessage({ body: messageData.message }, threadID);
+        if (messageData.attachment) {
+          const attachment = new MessageAttachment(messageData.attachment);
+          api.sendMessage({ body: messageData.message }, threadID);
+        } else {
+          api.sendMessage({ attachment, body: messageData.message }, threadID);
+        }
       });
     }
 
